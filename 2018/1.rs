19a16485bf9ce1c57dc7e547323822af01f7279a
Benjamin A. Stockwell <benjamin.stockwell@buzzfeed.com>
2019-12-05 00:43:30 -0600

    part 2 of 2018:1

--- 2018/1.rs
+++ 2018/1.rs
@@ -11,6 +11,35 @@ fn apply_frequency_changes(starting:i32, elements:Vec<String>) -> i32 {
     return result_frequency;
 }
 
+fn apply_frequency_changes_and_find_repeat(starting:i32, elements:Vec<String>) -> i32 {
+    let mut result_frequency:i32 = starting;
+
+    let mut result_freqs:Vec<i32> = vec![starting];
+
+    let mut found_repeat = false;
+
+    loop {
+        for el in &elements {
+            result_frequency = apply_frequency_change(result_frequency, el.parse::<i32>().unwrap());
+
+            if (&result_freqs).into_iter().any(|&f| f == result_frequency) {
+                found_repeat = true;
+                break;
+            }
+
+            result_freqs.push(result_frequency);
+
+            println!("current: {}", result_frequency);
+        }
+
+        if found_repeat {
+            break;
+        }
+    }
+
+    result_frequency
+}
+
 fn apply_frequency_change(current:i32, change:i32) -> i32 {
     return current + change;
 }
@@ -30,7 +59,9 @@ fn main() -> io::Result<()> {
         elements.push(l);
     }
 
-    let result = apply_frequency_changes(0, elements);
+    // let result = apply_frequency_changes(0, elements);
+
+    let result = apply_frequency_changes_and_find_repeat(0, elements);
 
     println!("result: {}", result);
 
@@ -39,18 +70,27 @@ fn main() -> io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use apply_frequency_change;
-    use apply_frequency_changes;
+    use *;
 
     #[test]
-    fn test_against_given_values_part_1() {
+    fn test_apply_frequency_change() {
         assert_eq!(apply_frequency_change(0, 1), 1);
         assert_eq!(apply_frequency_change(1, -2), -1);
         assert_eq!(apply_frequency_change(-1, 3), 2);
         assert_eq!(apply_frequency_change(2, 1), 3);
+    }
 
+    #[test]
+    fn test_apply_frequency_changes() {
         assert_eq!(apply_frequency_changes(0, vec![String::from("+1"), String::from("+1"), String::from("+1")]), 3);
         assert_eq!(apply_frequency_changes(0, vec![String::from("+1"), String::from("+1"), String::from("-2")]), 0);
         assert_eq!(apply_frequency_changes(0, vec![String::from("-1"), String::from("-2"), String::from("-3")]), -6);
+    }
+
+    #[test]
+    fn test_apply_frequency_changes_and_find_repeat() {
+        assert_eq!(apply_frequency_changes_and_find_repeat(0, vec![String::from("+1"), String::from("-1")]), 0);
+        assert_eq!(apply_frequency_changes_and_find_repeat(0, vec![String::from("+3"), String::from("+3"), String::from("+4"), String::from("-2"), String::from("-4")]), 10);
+        assert_eq!(apply_frequency_changes_and_find_repeat(0, vec![String::from("+7"), String::from("+7"), String::from("-2"), String::from("-7"), String::from("-4")]), 14);
     }
 }
